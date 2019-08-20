a3b2ccbbd9a1bd221d3676d4a6daa0750c740bba
Matthew Dias Prada <matthew.dias.prada@gmail.com>
2019-08-20 11:48:52 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -12,7 +12,7 @@ end
 def find_max_value(array)
   counter = 0
   max_val = array[0]
-  while array[counter] do
+  while array do
     if array[counter] > max_val
       max_val = array[counter]
     end
