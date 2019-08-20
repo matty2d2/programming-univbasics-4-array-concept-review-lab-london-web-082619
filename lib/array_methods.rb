d1d95bbfb1e89befe19c37c8507b3ca55282e6d6
Matthew Dias Prada <matthew.dias.prada@gmail.com>
2019-08-20 12:05:27 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -14,13 +14,13 @@ def find_max_value(array)
   max_val = array[0]
 
   array.each do |i|
-    
     if i > max_value
       max_value = i
+      position = counter
     end
     counter += 1
   end
-  
+  position
 end
 
 def find_min_value(array)
