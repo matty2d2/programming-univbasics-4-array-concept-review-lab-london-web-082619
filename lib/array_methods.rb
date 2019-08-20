972f1a59228b44ac30f91a91a2c18b7e9614bb34
Matthew Dias Prada <matthew.dias.prada@gmail.com>
2019-08-20 11:35:03 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -2,6 +2,8 @@ def find_element_index(array, value_to_find)
 counter = 0
 while array[counter] != value_to_find
   counter += 1
+  if counter == array.length do
+    
   return array[value_to_find]
 end
 
