c968fa8c8ca2bb91f090837683acbed6a0077c43
Matthew Dias Prada <matthew.dias.prada@gmail.com>
2019-08-20 11:36:38 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,10 +1,12 @@
 def find_element_index(array, value_to_find)
-counter = 0
-while array[counter] != value_to_find
-  counter += 1
-  if counter == array.length do
-    
-  return array[value_to_find]
+  counter = 0
+  while array[counter] != value_to_find
+    counter += 1
+    if counter == array.length
+      return nil
+    end
+  end
+  return counter
 end
 
 def find_max_value(array)
